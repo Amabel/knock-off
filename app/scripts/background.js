6cee71e2b542cc6de2ba4f0181f3a989be9ef409
Amabel <luoweibinb@gmail.com>
2019-06-20 08:58:38 +0900

    chore(ga): setup

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,6 +1,28 @@
-browser.runtime.onInstalled.addListener((details) => {
-  details
-  // console.log('previousVersion', details.previousVersion)
+import { TRACK_ID_DEV, TRACK_ID_PROD } from './shared/constants'
+
+// Setup GA
+chrome.management.getSelf((result) => {
+  let trackID
+
+  if (result.installType === 'development') {
+    trackID = TRACK_ID_DEV
+  } else {
+    trackID = TRACK_ID_PROD
+  }
+  setupGA(trackID)
 })
 
-// console.log(`'Allo 'Allo! Event Page`)
+function setupGA(trackID) {
+  let _gaq = []
+  _gaq.push(['_setAccount', trackID])
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    let ga = document.createElement('script')
+    ga.type = 'text/javascript'
+    ga.async = true
+    ga.src = 'https://ssl.google-analytics.com/ga.js'
+    let s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(ga, s)
+  })()
+}
---
+++ app/scripts/shared/constants.js
@@ -0,0 +1,2 @@
+export const TRACK_ID_PROD = 'UA-142457145-1'
+export const TRACK_ID_DEV = 'UA-142457145-2'
