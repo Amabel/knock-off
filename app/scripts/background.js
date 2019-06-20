3697c7899f14bd2aba229c0e74bd5cbf702062f2
Amabel <luoweibinb@gmail.com>
2019-06-20 18:42:45 +0900

    chore(ga): in progress

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,27 +2,27 @@ import { TRACK_ID_DEV, TRACK_ID_PROD } from './shared/constants'
 
 // Setup GA
 chrome.management.getSelf((result) => {
-  let trackID
+  let trackID = result.installType === 'development'
+    ? TRACK_ID_DEV
+    : TRACK_ID_PROD
 
-  if (result.installType === 'development') {
-    trackID = TRACK_ID_DEV
-  } else {
-    trackID = TRACK_ID_PROD
-  }
   setupGA(trackID)
 })
 
 function setupGA(trackID) {
+  // eslint-disable-next-line no-use-before-define
   let _gaq = []
   _gaq.push(['_setAccount', trackID])
   _gaq.push(['_trackPageview']);
 
   (function() {
-    let ga = document.createElement('script')
-    ga.type = 'text/javascript'
-    ga.async = true
+    let ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true
     ga.src = 'https://ssl.google-analytics.com/ga.js'
-    let s = document.getElementsByTagName('script')[0]
-    s.parentNode.insertBefore(ga, s)
+    let s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s)
   })()
+
+  // Receive events from content script and push to ga
+  chrome.runtime.onMessage.addListener(function(request) {
+    _gaq.push(['_trackEvent', request.eventCategory, request.eventAction])
+  })
 }
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,4 @@
-// console.log(`'Allo 'Allo! Content script`)
+init()
+
+function init() {
+}
