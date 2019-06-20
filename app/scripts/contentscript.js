b7a6340fc4ffc9a5bde3811dcb7197c93494d3e1
Amabel <luoweibinb@gmail.com>
2019-06-20 23:46:43 +0900

    feat(contentscript): add structure

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,4 +1,12 @@
-init()
 
-function init() {
+import { ATTENDANCE_PATH, MAN_HOUR_MANAGE_PATH  } from './shared/constants'
+import { initManHourManagePageAction } from './page-actions/man-hour-manage-page-action'
+
+const currentPath = window.location.pathname
+
+if (currentPath === ATTENDANCE_PATH) {
+  // attendance path actrion
+} else if (currentPath === MAN_HOUR_MANAGE_PATH) {
+  // man hour manage path action
+  initManHourManagePageAction()
 }
