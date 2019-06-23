8924507deee1aa25fa9070778696ce6c296682dc
Amabel <luoweibinb@gmail.com>
2019-06-23 16:52:13 +0900

    chore: clean up

--- app/scripts/shared/constants.js
+++ app/scripts/shared/constants.js
@@ -1,5 +1,2 @@
-export const TRACK_ID_PROD = 'UA-142457145-1'
-export const TRACK_ID_DEV = 'UA-142457145-2'
-
 export const ATTENDANCE_PATH = '/employee/attendance'
 export const MAN_HOUR_MANAGE_PATH = '/employee/man-hour-manage'
