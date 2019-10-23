302145db90d0a9a816ccbb09a32d6d718e96ff6b
Amabel <luoweibinb@gmail.com>
2019-10-23 13:53:48 +0900

    refactor: prevent prettifing specific codes

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -12,22 +12,21 @@ chrome.management.getSelf(result => {
   setupGA(trackID)
 })
 
+// prettier-ignore
 function setupGA(trackID) {
   (function(i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r
     // noinspection CommaExpressionJS
-    ;(i[r] =
-      i[r] ||
-      function() {
-        (i[r].q = i[r].q || []).push(arguments)
-      }),
-    (i[r].l = 1 * new Date())
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date()
     // noinspection CommaExpressionJS
-    ;(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0])
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0]
     a.async = 1
     a.src = g
     m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+  })(window, document, 'script',
+    'https://www.google-analytics.com/analytics.js', 'ga')
 
   ga('create', trackID, 'auto')
   ga('set', 'checkProtocolTask', function() {})
